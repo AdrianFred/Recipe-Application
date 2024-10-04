42c31fa94b22cd92bf7fca43c3283e4a3f9a137d
AdrianFred <adrian989@me.com>
2024-10-05 01:19:40 +0200

    Recipe Application

---
+++ app/layouts/default.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>
+    <Navigation />
+    <slot />
+  </div>
+</template>
---
+++ app/layouts/login.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Login Layout</h1>
+    <slot />
+  </div>
+</template>
